c521a4ff6d7d54eef4eef1a54f259e610108cbf0
Kuba <k.ciukiewicz@gmail.com>
2020-08-08 17:34:22 +0100

    Completed mastermind.rb and updated README.md with information about the game.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-The goal of this project is to build a Mastermind game from the command line where you have 12 turns to guess the secret code, starting with you guessing the computer’s random code.
+# Mastermind
 
+The goal of this project is to build a Mastermind game from the command line in Ruby.
+
+This implemetation is the classic version of the game with: 
+* 6 available colors 
+* code length of 4 
+* black and white feedback pins (There is nothing about the placement of the feedback pins to indicate which particular code pegs are meant.)
+* game length between 8 and 12 rounds
+
+A bit more about the game: https://en.wikipedia.org/wiki/Mastermind_(board_game)
+
+Game rules: https://magisterrex.files.wordpress.com/2014/07/mastermindrules.pdf
+
+To play, enter `$ ruby mastermind.rb` in the command line (you must have Ruby installed)
+
+ ## The game has 2 different modes:
+
+    1. The player is the code breaker. Here, the computer randomly generates the code which the player must then guess using the feedback provided.
+
+    2. The player is the code maker. Here, the player creates a code and the computer attempts to guess it using the feedback provided. The algorithm is described below.
+
+## Computer algorithm:
+
+    1. The computer starts by guessing all red in the first round.
+
+    2. It looks at the feedback given, and for every new feedback pin (black or white), it keeps that many red pins in the next guess, and populates the rest of the guess with orange.
+
+    3. The computer carries on cycling through all of the colors until 4 feedback pins have been achieved. It now knows exactly what color pins and how many of each color are in the code - what it does not know is the exact order of these pins.
+
+    4. For the remainder of the rounds, the computer randomly tries different orders of the pins, making sure that no guesses are repeated.
---
+++ mastermind.rb
@@ -0,0 +1,248 @@
+# frozen_string_literal: true
+
+# A class representing the game
+class Game
+  attr_reader :type
+  attr_reader :rounds
+  def initialize(type, rounds)
+    @colors = %w[red orange yellow green pink blue]
+    @color_code = {1 => 'red', 2 => 'orange', 3 => 'yellow', 4 => 'green', 5 => 'pink', 6 => 'blue'}
+    @type = type
+    @rounds = rounds
+  end
+
+  def show_colors
+    print 'Available colors: '
+    @colors.each { |color| print "#{color} " }
+    print "\n"
+  end
+
+  def ask_for_colors
+    colors_list = []
+    show_colors
+    color_number = 1
+    until color_number == 5
+      print "Enter color #{color_number}: "
+      input = gets.chomp.downcase
+      if @colors.include?(input)
+        color_number += 1
+        colors_list.push(input)
+      else
+        puts 'PLEASE ENTER A VALID COLOR!'
+      end
+    end
+    colors_list
+  end
+end
+
+# A class representing the game board
+class Board < Game
+  def initialize(code_sequence, type, rounds)
+    super(type, rounds)
+    @code_sequence = code_sequence
+    @guesses = []
+    @computer_guesses = []
+    @computer_confirmed = 0
+    @computer_background = 1
+    @feedback = []
+    @rounds = rounds
+  end
+
+  def add_to_guesses
+    @guesses.push(ask_for_colors)
+  end
+
+  def provide_feedback
+    guess = @guesses.last
+    assessment = []
+    code_copy = @code_sequence.dup
+    guess.each do |color|
+      position = code_copy.index(color)
+      unless position.nil?
+        code_copy.delete_at(position)
+        assessment.push('white')
+      end
+    end
+    counter = 0
+    guess.each_with_index do |color, index|
+      if color == @code_sequence[index]
+        assessment[counter] = 'black'
+        counter += 1
+      end
+    end
+    @feedback.push(assessment)
+  end
+
+  def show
+    @guesses.each_with_index do |guess, index|
+      guess.each { |color| print "#{color} " }
+      print '| '
+      @feedback[index].each { |color| print "#{color} " }
+      print "\n"
+    end
+  end
+
+  def check
+    @guesses.last == @code_sequence
+  end
+
+  def next_move
+    if @computer_confirmed == 3
+      guess = @computer_guesses.last.shuffle
+      while @computer_guesses.include?(guess)
+        guess = @computer_guesses.last.shuffle
+      end
+    else
+      if @computer_guesses.empty?
+        guess = [1, 1, 1, 1]
+      else
+        @computer_background += 1
+          if @feedback[-1].length > (@feedback.length < 2 ? 0 : @feedback[-2].length)
+            @computer_confirmed += @feedback[-1].length - (@feedback.length < 2 ? 0 : @feedback[-2].length)
+          end
+        guess = @computer_guesses.last
+        guess.each_with_index do |_element, index|
+          if index >= @computer_confirmed
+            guess[index] = @computer_background.dup
+          end
+        end
+      end
+    end
+    guess_colors = guess.map { |number| @color_code[number] }
+    @computer_guesses.push(guess.dup)
+    @guesses.push(guess_colors)
+  end
+end
+
+# A class representing the codemaker
+class CodeMaker < Game
+  attr_reader :name
+  attr_reader :code
+  def initialize(name, type, rounds)
+    super(type, rounds)
+    @name = name
+    @code = []
+    @type = type
+    @rounds = rounds
+  end
+
+  def generate_code
+    color_number = 1
+    until color_number == 5
+      random_number = Random.new.rand(6)
+      @code.push(@colors[random_number])
+      color_number += 1
+    end
+  end
+
+  def obtain_code
+    puts 'Enter your 4 color code!'
+    @code = ask_for_colors
+  end
+end
+
+# A class representing the codebreaker
+class CodeBreaker < Game
+  attr_reader :name
+  def initialize(name, type)
+    @name = name
+    @type = type
+  end
+end
+
+def mode
+  puts 'Who do you want to be?'
+  puts '1: Code Breaker'
+  puts '2: Code Maker'
+  correct_input = 0
+  until correct_input == 1
+    print 'Selection: '
+    game_mode = gets.chomp
+    if game_mode == '1' || game_mode == '2'
+      correct_input = 1
+    else 
+      puts 'PLEASE ENTER 1 OR 2'
+    end
+  end
+  game_mode
+end
+
+def obtain_name
+  puts 'What is your name?'
+  print 'Name: '
+  name = gets.chomp
+end
+
+def initialize_code_breaker(game)
+  code_breaker = if game.type == '1'
+                   CodeBreaker.new(obtain_name, game.type)
+                 else
+                   CodeBreaker.new('Computer', game.type)
+                 end
+  code_breaker
+end
+
+def initialize_code_maker(game)
+  if game.type == '1'
+    code_maker = CodeMaker.new('Computer', game.type, game.rounds)
+    code_maker.generate_code
+  else
+    code_maker = CodeMaker.new(obtain_name, game.type, game.rounds)
+    code_maker.obtain_code
+  end
+  code_maker
+end
+
+def round_1(board, round_counter)
+  puts 'AAAAAAAAAAAAAAAA'
+  puts "--- ROUND #{round_counter} ---"
+  board.show
+  board.add_to_guesses
+  board.provide_feedback
+end
+
+def round_2(board, round_counter)
+  board.next_move
+  board.provide_feedback
+  puts "--- ROUND #{round_counter} ---"
+  board.show
+end
+
+def number_of_rounds
+  puts 'How many rounds should the game have?'
+  correct_input = 0
+  until correct_input == 1
+    print 'Enter number from 8 - 12: '
+    rounds = gets.chomp.to_i
+    if rounds >= 8 && rounds <= 12
+      correct_input = 1
+    else 
+      puts 'PLEASE ENTER A NUMBER WITHIN RANGE'
+    end
+  end
+  rounds
+end
+
+def play
+  game = Game.new(mode, number_of_rounds)
+  code_breaker = initialize_code_breaker(game)
+  code_maker = initialize_code_maker(game)
+  board = Board.new(code_maker.code, game.type, game.rounds)
+  round_counter = 1
+  win_flag = false
+  while round_counter <= game.rounds && win_flag == false
+    game.type == '1' ? round_1(board, round_counter) : round_2(board, round_counter)
+    win_flag = board.check
+    round_counter += 1
+  end
+  if win_flag
+    puts "#{code_breaker.name} won!"
+  else
+    puts "#{code_maker.name} won!"
+  end
+  print 'The code was: '
+  code_maker.code.each { |color| print "#{color} " }
+  print "\n"
+end
+
+play
